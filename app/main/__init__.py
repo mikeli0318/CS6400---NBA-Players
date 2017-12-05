865ad7d28770cb0d5b595283f8e96e1decc27c04
Li, Shiyi <mikeli@lawn-128-61-71-49.lawn.gatech.edu>
2017-12-05 16:07:14 -0500

    setup almost done

---
+++ app/main/__init__.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+#instanciate Blueprint class, 2 params are bluprint name and the package/module located
+main = Blueprint('main', __name__)
+
+from . import views, errors

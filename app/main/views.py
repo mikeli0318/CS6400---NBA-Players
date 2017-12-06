537d1db14832b76fc2ff2420fd068700ae35a727
Li, Shiyi <mikeli@lishiyideMBP.attlocal.net>
2017-12-06 12:22:53 -0500

    fix blueprint registration

--- app/main/views.py
+++ app/main/views.py
@@ -1,40 +1,40 @@
 from flask import render_template, request
 #bluprint main
-from . import main
+from . import main, playerUtil, teamUtil
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('main.html')
 
 @main.route('/simPlayer', methods=['GET', 'POST'])
-def hhh():
+def getSimPlayer():
     name = str(request.args.get('name'))
     splitted = name.split("SPLITTER")
     playerName = splitted[0]
     for i in range(1, len(splitted) - 1):
         playerName = playerName + " " + splitted[i]
 
-    players = 1#
+    players = playerUtil.getSimilarPlayers(playerName)
     return render_template('simPlayers.html', result = players)
 
 @main.route('/simTeam', methods=['GET', 'POST'])
-def hhh():
+def getSimTeam():
     name = str(request.args.get('name'))
     splitted = name.split("SPLITTER")
     playerName = splitted[0]
     for i in range(1, len(splitted) - 1):
         playerName = playerName + " " + splitted[i]
 
-    players = ['A', 'B', 'C', 'D', playerName]
+    players = playerUtil.getSimilarTeams(playerName)
     return render_template('simTeam.html', result = players)
 
 @main.route('/teamPerform', methods=['GET', 'POST'])
-def hhh():
+def getTeamPerform():
     name = str(request.args.get('name'))
     splitted = name.split("SPLITTER")
-    playerName = splitted[0]
+    teamName = splitted[0]
     for i in range(1, len(splitted) - 1):
-        playerName = playerName + " " + splitted[i]
+        teamName = teamName + " " + splitted[i]
 
-    players = ['A', 'B', 'C', 'D', playerName]
+    players = teamUtil.getTeamPerformance(teamName)
     return render_template('teamPerform.html', result = players)

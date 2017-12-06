a4fa68c9fae85fb9cde2fe2a0e00fd3da75b1614
Li, Shiyi <mikeli@lishiyideMBP.attlocal.net>
2017-12-06 00:22:46 -0500

    workable index page with d3, redirect needed

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,12 @@ from flask import render_template
 #bluprint main
 from . import main
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return render_template('experiment_main.html')
+
+@main.route('/similarPlayer', methods=['GET', 'POST'])
+def hhh():
+    names = ['A', 'B', 'C', 'D']
+    return render_template('experiment_simPlayers.html', result = names)
+

29a845d0b271d0518c372b9e3b5a0b0422b6db3c
Li, Shiyi <mikeli@lawn-128-61-78-106.lawn.gatech.edu>
2017-12-06 16:50:16 -0500

    similar players done

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 #instanciate Blueprint class, 2 params are bluprint name and the package/module located
 main = Blueprint('main', __name__)
 
-from . import views, errors, DBConnectionConfig, Similarity, teamUtil
+from . import views, errors, DBConnectionConfig, Similarity, teamUtil, wikiUtil
